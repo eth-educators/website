ab505a728e0fee273c374eb46d8c42145acfee77
hanniabu <hanniabu@gmail.com>
2023-04-26 10:48:02 -0400

    improve redirect check and write readme

--- _includes/js/rhinoReviewRedirect.js
+++ _includes/js/rhinoReviewRedirect.js
@@ -1,9 +1,9 @@
 let params = getQueryParameters();
-let redirectRoot = "https%3A%2F%2Frhinoreview.substack.com"
+let redirectRoot = "https%3A%2F%2Frhinoreview.substack.com";
 
-// If url param and contains redirect root, then redirect, otherwise clear params 
-if ("url" in params && params.url.includes(redirectRoot)) {
-  let url = decodeURIComponent(params.url)
+// If url param and starts with redirect root, then redirect, otherwise clear params 
+if ("url" in params && params.url.startsWith(redirectRoot)) {
+  let url = decodeURIComponent(params.url);
   window.location.replace(url);
 } else {
   let url = window.location.href.split("?")[0];
